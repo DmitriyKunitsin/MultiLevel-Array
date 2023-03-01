f2580acb3903b7fd661429a36908be6ce80f254d
kunicind1996@gmail.com <kunicind1996@gmail.com>
2023-03-01 22:46:52 +0700

    Добавьте файлы проекта.

---
+++ Трехмерные массивы(Многомерные массивы)/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Трехмерные_массивы_Многомерные_массивы_
+{
+    internal class Program
+    {
+        // Если я ковыряюсь в массиве [,,,,,], то октуален метод GetLength(..), т.к. им я перемещаюсь
+        // по измерениям массива, а если массив не однородный(ступенчатый, зубчатый), то
+        // надо перебирать каждый массив методом Length, т.к. я смотрю его длину
+        static void Main(string[] args)
+        {
+            int[,,,] NewArray = new int[2,2,5,5];
+            Random RandomNumberArray= new Random();
+            for (int p = 0; p < NewArray.GetLength(0); p++)
+            {
+                for (int u = 0; u < NewArray.GetLength(1); u++)
+                {
+                    for (int i = 0; i < NewArray.GetLength(2); i++)
+                    {
+                        for (int o = 0; o < NewArray.GetLength(3); o++)
+                        {
+                            NewArray[p, u, i, o] = RandomNumberArray.Next(1,50);
+                        }
+                    }
+                }
+            }
+            for (int p = 0; p < NewArray.GetLength(0); p++)
+            {
+                Console.WriteLine("Book # :"+ (p + 1));
+                for (int u = 0; u < NewArray.GetLength(1); u++)
+                {
+                    Console.WriteLine("Page # :" + (u + 1));
+                    for (int i = 0; i < NewArray.GetLength(2); i++)
+                    {
+                        for (int o = 0; o < NewArray.GetLength(3); o++)
+                        {
+                            Console.Write(NewArray[p,u, i, o] + " "); ;
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+            } 
+        }
+    }
+}
---
+++ Трехмерный зубчатый массив/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Трехмерный_зубчатый_массив
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random RandomNumberArray = new Random();
+            int[][][] NewArray = new int[RandomNumberArray.Next(2, 5)][][];
+            for (int u = 0; u < NewArray.Length; u++)
+            {
+                NewArray[u] = new int [RandomNumberArray.Next(2, 5)][];
+                for (int i = 0; i < NewArray[u].Length; i++)
+                {
+                    NewArray[u][i] = new int[RandomNumberArray.Next(4, 8)]; 
+                    for (int o = 0; o < NewArray[u][i].Length; o++)
+                    {
+                        NewArray[u][i][o] = RandomNumberArray.Next(8, 150);
+                    }
+                }
+            }
+            for (int u = 0; u < NewArray.Length; u++)
+            {
+                Console.WriteLine("Book # :" + (u+1));
+                for (int i = 0; i < NewArray[u].Length; i++)
+                {
+                    Console.WriteLine("Page # :" + (i + 1));
+                    for (int o = 0; o < NewArray[u][i].Length; o++)
+                    {
+                        Console.Write(NewArray[u][i][o]+ " "); ;
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
